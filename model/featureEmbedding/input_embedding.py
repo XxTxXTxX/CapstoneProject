d874c9d2f6f3bd42b5e5f0b2d9ad937c05d52c8b
Yuxiao Tian <jameslikelywood@gmail.com>
2025-03-26 22:34:07 -0400

    1

--- model/featureEmbedding/input_embedding.py
+++ model/featureEmbedding/input_embedding.py
@@ -2,31 +2,10 @@ import torch
 from torch import nn
 
 class InputEmbedder(nn.Module):
-    """
-    Implements Algorithm 3 and Algorithm 4.
-    """
-
     def __init__(self, c_m, c_z, tf_dim, msa_feat_dim=49, vbins=32):
-        """
-        Initializes the InputEmbedder.
-
-        Args:
-            c_m (int): Embedding dimension of the MSA representation.
-            c_z (int): Embedding dimension of the pair representation.
-            tf_dim (int): Embedding dimension of target_feat.
-            msa_feat_dim (int, optional): Embedding dimension of the MSA feature. 
-                Defaults to 49.
-            vbins (int, optional): Determines the bins for relpos as 
-                (-vbins, -vbins+1,...,vbins). Defaults to 32.
-        """
         super().__init__()
         self.tf_dim = tf_dim
         self.vbins = vbins
-        
-        # Initialize the modules linear_tf_z_i, linear_tf_z_j,linear_tf_m, linear_msa_m (from Algorithm 3) and linear_rel_pos (from Algorithm 4).
-
-        # Note the difference between the MSA feature (as created during feature extraction) and the MSA representation m that is used throughout the Evoformer.
- 
         self.linear_tf_z_i = nn.Linear(tf_dim, c_z)
         self.linear_tf_z_j = nn.Linear(tf_dim, c_z)
         self.linear_tf_m = nn.Linear(tf_dim, c_m)
@@ -34,32 +13,8 @@ class InputEmbedder(nn.Module):
         self.linear_relpos = nn.Linear(2*vbins+1, c_z)
 
     def relpos(self, residue_index):
-        """
-        Implements Algorithm 4.
-
-        Args:
-            residue_index (torch.tensor): Index of the residue in the original amino
-                acid sequence. In this context, this is simply [0,... N_res-1].
-
-        Returns:
-            tuple: Tuple consisting of the embedded MSA representation m and 
-                pair representation z.
-        """
-
         out = None
         dtype = self.linear_relpos.weight.dtype
-
-        # Implement Algorithm 4. Since the residue index is just a number, we can directly use the shifted d_ij as class labels.                #
-        # You can follow these steps:
-        #   * Cast residue_index to long.                                        #
-        #   * unsqueeze residue_index accordingly to calculate the outer         #
-        #      difference d_ij.                                                  #
-        #   * use torch.clamp to clamp d_ij between -self.vbins and self.vbins.  #
-        #   * offset the clamped d_ij by self.vbins, so that it is in the range  #
-        #      [0, 2*vbins] instead of [-vbins, vbins].                          #
-        #   * use nn.functional.one_hot to convert the class labels into         #
-        #      one-hot encodings.                                                #
-        #   * use the linear module to create the output embedding.
         residue_index = residue_index.long()
         d = residue_index.unsqueeze(-1) - residue_index.unsqueeze(-2)
         d = torch.clamp(d, -self.vbins, self.vbins) + self.vbins
@@ -80,11 +35,6 @@ class InputEmbedder(nn.Module):
         pH_range = [0, 14],
         temp_range = [273, 300]
     ):
-        """
-        Rotate the C_z embedding dimension in the input of shape (N_res, C_z)
-            - rotation happens before outer sum between "a" and "b"
-        returns rotated tensor of shape (N_res, C_z)
-        """
         input = input.squeeze(dim=0)
         device = input.device
         N_res, C_z = input.shape
@@ -129,19 +79,6 @@ class InputEmbedder(nn.Module):
 
 
     def forward(self, batch):
-        """
-        Implements the forward pass for Algorithm 3.
-
-        Args:
-            batch (dict): Feature dictionary with the following entries:
-                * msa_feat: Initial MSA feature of shape (*, N_seq, N_res, msa_feat_dim).
-                * target_feat: Target feature of shape (*, N_res, tf_dim).
-                * residue_index: Residue index of shape (*, N_res)
-
-        Returns:
-            tuple: Tuple consisting of the MSA representation m and the pair representation z.
-        """
-
         m = None
         z = None
 
@@ -151,11 +88,6 @@ class InputEmbedder(nn.Module):
         target_feat = batch['target_feat']
         # N_res, 
         residue_index = batch['residue_index']
-
-        # Implement the forward pass for Algorithm 3. For the calculation of the outer sum in line 2, the embeddings a and b must be unsqueezed correctly to allow for broadcasting along the N_res dim.
-        
-        # Note: For batched use, target_feat must be unsqueezed after the computation of a and b and before the computation of m, to match the number of dimensions of msa_feat.
-
         # N_res, 21 -> N_res, C_z
         a = self.linear_tf_z_i(target_feat)
         # N_res, 21 -> N_res, C_z
@@ -169,7 +101,6 @@ class InputEmbedder(nn.Module):
         z = a_rotated.unsqueeze(-2) + b_rotated.unsqueeze(-3)
         # N_res, N_res, C_z
         z = z + self.relpos(residue_index.squeeze(0))        
-        #TODO: Add PH and temperature rotations
         
         # (1, N_res, 21)
         target_feat = target_feat.unsqueeze(-3)
